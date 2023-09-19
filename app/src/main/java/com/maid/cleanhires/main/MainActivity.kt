f82768a1bf3799abb99c85b283dd61ce739ca7b5
RohanRamola <rohanramola63@gmail.com>
2023-09-20 03:23:02 +0530

    UI Modifications

--- app/src/main/java/com/maid/cleanhires/main/MainActivity.kt
+++ app/src/main/java/com/maid/cleanhires/main/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.maid.cleanhires.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
 import com.maid.cleanhires.R
+import com.maid.cleanhires.databinding.ActivityMainBinding
+import com.maid.cleanhires.ui.activities.HomeActivity
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler(Looper.getMainLooper()).postDelayed(
+            Runnable {
+                Intent(this@MainActivity, HomeActivity::class.java).also {
+                    startActivity(it)
+                }
+            }
+        , 4000)
     }
 }
--- app/src/main/java/com/maid/cleanhires/ui/activities/HomeActivity.kt
+++ app/src/main/java/com/maid/cleanhires/ui/activities/HomeActivity.kt
@@ -1,12 +1,46 @@
 package com.maid.cleanhires.ui.activities
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.MenuItem
+import android.view.View
+import android.view.WindowManager
+import androidx.fragment.app.Fragment
 import com.maid.cleanhires.R
+import com.maid.cleanhires.databinding.ActivityHomeBinding
+import com.maid.cleanhires.ui.fragments.ProfileFragment
+import com.maid.cleanhires.ui.fragments.ServicesFragment
 
 class HomeActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        window.statusBarColor = Color.WHITE
+        loadFragment(ServicesFragment())
+
+        binding.bottomNavigationView.setOnItemSelectedListener { 
+            when (it.itemId){
+                R.id.HomePage -> {
+                    loadFragment(ServicesFragment())
+                    true
+                }
+                else -> {
+                    loadFragment(ProfileFragment())
+                    true
+                }
+            }
+        }
+    }
+
+    private  fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.flFrameLayout,fragment)
+        transaction.commit()
     }
 }
