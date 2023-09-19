ceeb7af61e64681f7537b988dc43255c3f78596f
RohanRamola <rohanramola63@gmail.com>
2023-09-19 16:34:02 +0530

    First Commit

---
+++ app/src/main/java/com/maid/cleanhires/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.maid.cleanhires.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.maid.cleanhires.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/maid/cleanhires/ui/activities/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.maid.cleanhires.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.maid.cleanhires.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
